620b7175a8a29cf799f0c25462b2487e23179f56
Roland Kuhn <rk@rkuhn.info>
2020-07-29 19:40:27 +0200

    first sketch

--- index.js
+++ index.js
@@ -9,8 +9,39 @@ async function run() {
     const token = core.getInput('token');
     console.log('got token');
 
-    console.log(github.context);
-    const octo = github.getOctokit(token);
+    const octo = github.getOctokit(token, {
+      // to get access to issue timeline events
+      previews: ['mockingbird-preview'],
+    });
+
+    const { owner, repo } = github.context.repo;
+    const { number, state } = github.context.payload.issue;
+    console.log(`issue ${number} is ${state}`);
+
+    const events = await octo.issues.listEventsForTimeline({ owner, repo, issue_number: number });
+    const epics = events
+      .filter(
+        (ev) =>
+          ev.event === 'cross-referenced' &&
+          ev.source &&
+          ev.source.issue &&
+          ev.source.issue.labels.some((l) => l.name === label),
+      )
+      .map((ev) => ev.source.issue);
+    console.log(`${epics.length} epics to update`);
+
+    const setTo = state === 'closed' ? '- [x]' : '- [ ]';
+    const prefix = setTo.length;
+    const pattern = new RegExp(`- \\[([ x])].*#${number}`, 'g');
+    await Promise.all(
+      epics.map(async (epic) => {
+        console.log(`updating ${epic.number}`);
+        const body = epic.body;
+        body.replace(pattern, (s) => setTo + s.substr(prefix));
+        return await octo.issues.update({ owner, repo, issue_number: epic.number, body });
+      }),
+    );
+    console.log('done');
   } catch (e) {
     core.setFailed(e.message);
   }
