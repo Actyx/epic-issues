12669e0c5bbd1ddd6499a5f1bc03fd17df3b3b5d
Roland Kuhn <rk@rkuhn.info>
2020-07-29 18:39:17 +0200

    trying the api

---
+++ index.js
@@ -0,0 +1,17 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  try {
+    const label = core.getInput('label');
+    console.log('label', label);
+
+    const token = core.getInput('token');
+    console.log('got token');
+
+    console.log(github.context);
+    const octo = github.getOctokit(token);
+  } catch (e) {
+    core.setFailed(e.message);
+  }
+}
